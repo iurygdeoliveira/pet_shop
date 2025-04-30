ee9d704528f01fb7a84844f9a9ac62d19287c608
iury.oliveira <iury.oliveira@ifto.edu.br>
2025-04-30 11:05:04 +0000

    atualizando traduçao

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use Filament\Tables\Columns\Column;
+use Filament\Forms\Components\Field;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -23,6 +25,15 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+        ->bootUsing(function (): void {
+            Field::configureUsing(function (Field $field): void {
+                $field->translateLabel();
+            });
+
+            Column::configureUsing(function (Column $column): void {
+                $column->translateLabel();
+            });
+        })
             ->default()
             ->id('admin')
             ->path('admin')
@@ -38,8 +49,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                //Widgets\AccountWidget::class,
+                //Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
